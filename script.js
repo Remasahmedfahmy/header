d3c752caaf1732a1be401ae4ba0e9dc92482a114
Remas ahmed <remasahmed232100@gmail.com>
2024-08-21 00:54:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+let first = document.getElementById('first')
+let second = document.getElementById('second')
+let third = document.getElementById('third')
+let fourth = document.getElementById('fourth')
+let group = document.getElementById('group')
+
+
+
+first.onclick = function () {
+    group.classList.toggle("active")
+}
+
+
